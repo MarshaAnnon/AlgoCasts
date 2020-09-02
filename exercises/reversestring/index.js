94b4374537b1b0866306c68e4aa74c7696b3ed66
marsha <marshaannon@gmail.com>
2020-09-02 12:39:23 -0400

    complete exercises

---
+++ exercises/reversestring/index.js
@@ -0,0 +1,17 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str) {
+    if (str === "") {
+        return "";
+    } else {
+        return reverse(str.substr(1)) + str.charAt(0);
+    }
+}
+    
+module.exports = reverse;
